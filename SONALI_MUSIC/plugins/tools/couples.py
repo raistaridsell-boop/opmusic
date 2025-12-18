7578aac05682981e28e080566a05e40a97a06697
TEAM-ISHA <payalgaming1008@gmail.com>
2025-12-18 09:07:37 +0530

    Team-Isha

--- SONALI_MUSIC/plugins/tools/couples.py
+++ SONALI_MUSIC/plugins/tools/couples.py
@@ -15,7 +15,7 @@ POLICE = [
     [
         InlineKeyboardButton(
             text="• ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ •",
-            url=f"https://t.me/Hena_robot?startgroup=true",
+            url=f"https://t.me/ytxmusic_bot?startgroup=true",
         ),
     ],
 ]
